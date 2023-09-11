e3bca69fcf2845face2980669ddd5122675f0112
Seongjin <43747041+Sungjin01@users.noreply.github.com>
2023-09-11 07:46:13 +0000

    header menu

--- webgamecollection/src/App.js
+++ webgamecollection/src/App.js
@@ -6,17 +6,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <div className="App-menu">
+          <div>
+            Arcade
+          </div>
+          <div>
+            BoardGame
+          </div>
+          <div>
+            Action
+          </div>
+          <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          >
+            Learn React
+          </a>
+        </div>
       </header>
     </div>
   );
