537f9ec5f56c17d0817b9670eca50db63ab96b38
Seongjin <43747041+Sungjin01@users.noreply.github.com>
2023-09-11 08:00:01 +0000

    header menu

--- webgamecollection/src/App.js
+++ webgamecollection/src/App.js
@@ -16,14 +16,17 @@ function App() {
           <div>
             Action
           </div>
-          <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+            <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </div>
+          
         </div>
       </header>
     </div>
